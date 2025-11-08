057f4d87ffd981e42f264c901f0b921f1cc770bc
nananono <abcdbe007@gmail.com>
2025-11-08 21:02:01 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+```jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // optional, for Tailwind or custom styles
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);
+```
